f4e5ceb71462577223c98ed74a3d91afdd1b9125
chettas <dineshdhonidk29@gmail.com>
2024-06-20 22:21:39 +0530

    first commit

---
+++ src/operators.java
@@ -0,0 +1,28 @@
+public class operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if (a>b) {
+            System.out.println("a is greater than b");
+        }
+        else if (a==b){
+            System .out .println("a is equal to b");
+        }
+        else  if (a<b){
+            System .out .println(" a is lesser than b");
+        }
+         if (a<=b){
+            System .out .println("a is lesser than or equal to b");
+        }
+        else if (a>=b){
+            System.out.println("a is greater than or equal to b");
+        }
+         if (a!=b){
+            System.out.println("a is not equal to B");
+        }
+        else {
+            System.out.println("Enter a value");
+       }
+    }
+}
