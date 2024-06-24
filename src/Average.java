b2ca53532b8538169a6707862380529387973162
chettas <dineshdhonidk29@gmail.com>
2024-06-24 22:59:10 +0530

    adding datatypes

---
+++ src/Average.java
@@ -0,0 +1,6 @@
+public class Average {
+    public static void main (String [] args){
+        double average= (45+25+65)/3;
+        System.out.println("the average of the number is:"+average);
+    }
+}
